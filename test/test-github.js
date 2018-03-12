73f7cee8572d8bdd40ff41a430e37993ef2466f1
Tomislav Jovanovic <zombie@users.noreply.github.com>
2018-03-12 01:19:43 +0100

    heh

--- test/test-github.js
+++ test/test-github.js
@@ -1,36 +1,37 @@
 
 const {resolve} = require("path");
-const extension = resolve(__dirname, "../ext/");
+const extension = resolve(__dirname, "..");
 
 const assert = require('assert');
 const puppeteer = require('puppeteer');
 
-describe('Execute Actions', function () {
-  this.timeout(90000);
+let browser;
+
+before(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      '--no-sandbox',
+      `--load-extension=${extension}`,
+      `--disable-extensions-except=${extension}`,
+    ],
+  });
+});
+
+describe('Execute Actions', function() {
+  this.timeout(10000);
   it('should run the scenario with Puppeteer', async function() {
-    const browser = await puppeteer.launch({
-      args: [
-        `--disable-extensions-except=${extension}`,
-        `--load-extension=${extension}`,
-        '--disable-setuid-sandbox',
-        '--no-sandbox',
-      ],
-      headless: false,
-    });
-    // console.log("bb", browser);
-    
     const page = await browser.newPage();
     await page.goto('https://github.com/zombie/blind-reviews/pull/1');
-    const title = await page.title();
-    const x = await page.$("#br-toggle");
-    const bb = x && await x.boundingBox();
+    
+
+    const br = await page.$("#br-toggle");
+    const bb = await br.boundingBox();
 
     assert(bb.height > 20, "tall");    
     assert(bb.width > 30, "wide");    
 
-    await new Promise(r => setTimeout(r, 10000));
-    browser.close();
-  
+    browser.close();  
     console.log(' 🎉 ');
   });
 });
